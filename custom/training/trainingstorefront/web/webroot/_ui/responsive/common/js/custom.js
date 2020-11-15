d2b96494b165dac17aabe4ab85ec44cddde67b47
Jonathan Alva <jonathan.alva97@yahoo.com>
2020-11-15 18:03:37 +0700

    Adding Slick JS and Customize Banner Carousel

---
+++ custom/training/trainingstorefront/web/webroot/_ui/responsive/common/js/custom.js
@@ -0,0 +1,30 @@
+if (!window.demo) {
+    window.demo = {};
+}
+var demo = window.demo;
+
+if (!window.ACC) {
+    window.ACC = {config: {encodedContextPath: {}}};
+}
+(function ($) {
+    demo.global = {
+        home: function () {
+            var mainBanner = $(".main-banner");
+            mainBanner.slick({
+                arrows: false,
+                dots: true,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 1,
+                adaptiveHeight: true,
+                swipe: true,
+                swipeToSlide: true
+            });
+        },
+        init: function () {
+            var _self = demo.global;
+            _self.home();
+        }
+    };
+    demo.global.init();
+})(jQuery);
