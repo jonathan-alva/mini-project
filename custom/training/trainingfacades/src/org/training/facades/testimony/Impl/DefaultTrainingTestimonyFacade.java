09f4b50ea789706992faba41c02cbca04aa28390
Muhammad Ilmi Fauzan <m.ilmi.fauzan@gmail.com>
2020-12-01 11:47:17 +0800

    rev Testimony

--- custom/training/trainingfacades/src/org/training/facades/testimony/Impl/DefaultTrainingTestimonyFacade.java
+++ custom/training/trainingfacades/src/org/training/facades/testimony/Impl/DefaultTrainingTestimonyFacade.java
@@ -1,19 +1,14 @@
 package org.training.facades.testimony.Impl;
 
-import de.hybris.platform.cmsfacades.data.ItemData;
-import de.hybris.platform.commercefacades.product.data.ProductData;
-import de.hybris.platform.core.model.ItemModel;
-import de.hybris.platform.core.model.product.ProductModel;
+
 import de.hybris.platform.servicelayer.dto.converter.Converter;
 import org.apache.commons.collections.CollectionUtils;
 import org.training.core.data.beans.TestimonyData;
 import org.training.core.model.TrainingTestimonyModel;
-import org.training.core.service.TrainingProductService;
 import org.training.core.service.TrainingTestimonyService;
 import org.training.facades.testimony.TrainingTestimonyFacade;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -29,11 +24,7 @@ public class DefaultTrainingTestimonyFacade implements TrainingTestimonyFacade {
     public List<TestimonyData> getAllTestimonyModels() {
         List<TrainingTestimonyModel> trainingTestimonyModels = trainingTestimonyService.getAllTestimonyModels();
         if(CollectionUtils.isNotEmpty(trainingTestimonyModels)){
-            final List<TestimonyData> testimonyDataList = new ArrayList<>();
-            for(final TrainingTestimonyModel testimonyModel:trainingTestimonyModels){
-                testimonyDataList.add(testimonyDataConverter.convert(testimonyModel));
-            }
-            return testimonyDataList;
+            return testimonyDataConverter.convertAll(trainingTestimonyModels);
         }
         return Collections.emptyList();
     }
